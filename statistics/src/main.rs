746a24fc439130f8955ead96fcd8d8c108080bf7
mikedel <mcdelsi@gmail.com>
2023-10-26 22:16:55 -0400

    added first challenge from chapter 7

---
+++ statistics/src/main.rs
@@ -0,0 +1,32 @@
+fn median(v: &Vec<i32>) -> i32 {
+    let mut v = v.clone();
+    v.sort();
+    let mid = v.len() / 2;
+    v[mid]
+}
+
+fn mode(v: &Vec<i32>) -> i32 {
+    let mut counts = std::collections::HashMap::new();
+    for &i in v {
+        let count = counts.entry(i).or_insert(0);
+        *count += 1;
+    }
+    let mut mode = 0;
+    let mut max_count = 0;
+    for (k, v) in counts {
+        if v > max_count {
+            max_count = v;
+            mode = k;
+        }
+    }
+    mode
+}
+
+fn main() {
+    println!("Start");
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 8, 8];
+    println!("Median: {}", median(&v));
+    println!("Mode: {}", mode(&v));
+
+    println!("Hello, world!");
+}
